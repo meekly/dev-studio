b96a642fc1ecb1c4a65ad49032ce43819faa42b6
Valentine Kiselev <mrexox@yahoo.com>
2017-04-10 22:29:55 +0300

    menu (has bugs)

--- app/assets/javascripts/private.js
+++ app/assets/javascripts/private.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
-	$("a.menu__projects[data-remote]").on("ajax:success", function(e, data, status, xhr){
+	$.ajax({
+		url: "/projects.json",
+		success: function(data) {
 		var projects = document.getElementsByClassName('projects')[0];
-		projects.innerHTML = "";
-		$(data).each(function(index, project) {
-			if (!project.finished) {
-				var li = document.createElement('li');
-				var href = document.createElement('a');
-				href.href = '/projects/' + project.id;
-				href.innerHTML = project.name;
-				li.appendChild(href);
-				projects.appendChild(li);
+			$(data).each(function(index, project) {
+				if (!project.finished) {
+					var li = document.createElement('li');
+					var href = document.createElement('a');
+					href.href = '/projects/' + project.id;
+					href.innerHTML = project.name;
+					li.appendChild(href);
+					projects.appendChild(li);
 			}
 		});
+		}
+	});
+	$("li.menu__projects").on('mouseover', function() {
+		var projects = document.getElementsByClassName('projects')[0];
 		projects.style.display = 'block';
-	}).on("ajax:error", function(e, data, status, error) { alert(error);});;
-
+	});
+	$(".projects").on('mouseleave', function() {
+		var projects = document.getElementsByClassName('projects')[0];
+		projects.style.display = 'none';
+	});
 
 	$(document).mouseup(function(e) {
 		var projects = document.getElementsByClassName('projects')[0];
 
-		if (!e.target.className !== 'projects' 
-				)
+		if (!e.target.className !== 'projects')
 		{
 			projects.style.display = 'none';
 		}
